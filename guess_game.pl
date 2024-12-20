a6f909d49822a4bf4ff9b42c538e3e2d376a40a0
Harsh Gondaliya <157871191+HarshGondaliya78@users.noreply.github.com>
2024-12-20 11:15:18 +0530

    resolve conflicts

--- guess_game.pl
+++ guess_game.pl
@@ -1,20 +1,25 @@
 use strict;
 use warnings;
 
+# Define the range for the random number
 my $min = 0;
 my $max = 100;
 my $guess=0;
 
+# Generate a random secret number between $min and $max
 my $secrate_number =  int ($min+rand($max-$min+1));
 
 my $attempt = 0;
+
 print "Enter your choice number\n";
 while(1){
+# Read user input and remove the newline character at the end
   chomp(my $guess_number=<STDIN>);
   $attempt++;
 
   print "attempt $attempt : $guess_number\n";
-   
+
+# Check if the guess is within the range
    if($min <= $guess_number <=$max){ 
       if($guess_number==$secrate_number){
         print "Congratulations !! You have guess Right Number in $attempt attempt\n";
@@ -33,6 +38,7 @@ while(1){
 }
 
 print "Original Number is $secrate_number\n";
+#Final message
 print "Thank You For Playing this game ! I hope You have Enjoy this Game.  \n";
 print "Game Over";
 exit;
