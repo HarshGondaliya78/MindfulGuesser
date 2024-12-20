9505ebfff2f415d3d52896dce8528194c71cfb29
Harsh Gondaliya <157871191+HarshGondaliya78@users.noreply.github.com>
2024-12-20 11:08:15 +0530

    initial commit

--- test.pl
+++ test.pl
@@ -1,15 +1,11 @@
 use strict;
 use warnings;
 
+#test for secrate number is lies within range.
+print "Running tests...\n";
 my $min = 0;
 my $max = 100;
 
 my $secrate_number =  int ($min+rand($max-$min+1));
-print "Secrate Number is : $secrate_number\n";
-   if($min<=$secrate_number<=$max){
-      print "True";
-   }
-   else{
-       print "False";
-   }
-exit(0);
+print "Random number within range: ", $secrate_number >= $min && $secrate_number <= $max ? "PASS\n" : "FAIL\n";
+
